127794fdfbb5e510f9a7f8e7faa43ccdfa798891
Bishal Poudel <poudelbishal2323@gmail.com>
2026-06-06 22:26:33 +0530

    Update project files

---
+++ lib/screens/login.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  const Login({super.key});
+
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  //From Controllers and State
+  
+
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
